a9fde65f9cad0d879c367462ed2ba77c53c5f068
Burak Guney <burakguney.computer@gmail.com>
2023-05-15 15:38:28 +0300

    new feautures added

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-const port = 8080
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const userModel = require('./schemas/User')
 
+dotenv.config()
+const port = process.env.PORT
+const mongodb = process.env.MONGODB
+
+const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const { username, password } = req.body
 
-    res.json({ requsetData: { username, password } })
+    try {
+        const user = await userModel.create({ username, password })
+
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(400).json(error)
+    }
 })
+
+mongoose.connect(mongodb)
+    .then(() => {
+        console.log("Database connected.")
+    }).catch((err) => {
+        console.log(err.message)
+        throw err;
+    })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
